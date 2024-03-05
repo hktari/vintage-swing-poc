a31ec68ab06ae5b1cf79df262d3552289691364f
hktari <bkamnik1995@gmail.com>
2024-03-05 11:08:46 +0100

    layout driver cell

---
+++ src/components/DriversTable.jsx
@@ -0,0 +1,91 @@
+import React from "react"
+
+import Table from "@mui/material/Table"
+import TableHead from "@mui/material/TableHead"
+import TableBody from "@mui/material/TableBody"
+import TableCell from "@mui/material/TableCell"
+import TableContainer from "@mui/material/TableContainer"
+import TableRow from "@mui/material/TableRow"
+import Paper from "@mui/material/Paper"
+import Avatar from "@mui/material/Avatar"
+import MessageIcon from "@mui/icons-material/Message"
+import IconButton from "@mui/material/IconButton"
+import Typography from "@mui/material/Typography"
+import { Box } from "@mui/material"
+
+const DriversTable = () => {
+  function createData(
+    driverName,
+    driverImgSrc,
+    departure,
+    arrival,
+    availableSeats
+  ) {
+    return { driverName, driverImgSrc, departure, arrival, availableSeats }
+  }
+
+  const rows = [
+    createData(
+      "Marko",
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "17:00 Celje",
+      "00:00 Celje",
+      2
+    ),
+    createData(
+      "Vesna",
+      "https://plus.unsplash.com/premium_photo-1683121769247-7824fdc324de?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D",
+      "19:00 Kranj",
+      "01:00 Kranj",
+      1
+    ),
+  ]
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell align="center">Voznik</TableCell>
+            <TableCell align="center">Odhod</TableCell>
+            <TableCell align="center">Prihod</TableCell>
+            <TableCell align="center">Št. prostih mest</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                <IconButton aria-label="message">
+                  <MessageIcon />
+                </IconButton>
+              </TableCell>
+              <TableCell align="center">
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  flexDirection={"column"}
+                >
+                  {" "}
+                  <Avatar alt={row.name} src={row.driverImgSrc} />
+                  <Typography variant="subtitle1" sx={{ mt: 1 }}>
+                    {row.driverName}
+                  </Typography>
+                </Box>
+              </TableCell>
+              <TableCell align="center">{row.departure}</TableCell>
+              <TableCell align="center">{row.arrival}</TableCell>
+              <TableCell align="center">{row.availableSeats}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default DriversTable
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import Typography from "@mui/material/Typography"
 import Layout from "../components/Layout"
 
 import Table from "@mui/material/Table"
+import TableHead from "@mui/material/TableHead"
 import TableBody from "@mui/material/TableBody"
 import TableCell from "@mui/material/TableCell"
 import TableContainer from "@mui/material/TableContainer"
@@ -22,6 +23,7 @@ import CallIcon from "@mui/icons-material/Call"
 
 import users from "../../content/users.json"
 import Section from "../components/Section"
+import DriversTable from "../components/DriversTable"
 
 export default function Index() {
   return (
@@ -104,45 +106,7 @@ export default function Index() {
         </Section>
 
         <Section title={"Prevozi"}>
-          <List dense={false}>
-            {users.map(user => (
-              <ListItem
-                secondaryAction={
-                  <IconButton edge="end" aria-label="delete">
-                    <CallIcon />
-                  </IconButton>
-                }
-              >
-                <ListItemAvatar>
-                  <Avatar alt={user.name} src={user.imgSrc} />
-                </ListItemAvatar>
-                <ListItemText primary={user.name} secondary={user.level} />
-                <ListItemText primary={user.status} />
-              </ListItem>
-            ))}
-            <ListItem
-              secondaryAction={
-                <IconButton edge="end" aria-label="delete">
-                  <CallIcon />
-                </IconButton>
-              }
-            >
-              <ListItemAvatar>
-                <Avatar
-                  alt={"Boštjan"}
-                  src="https://plus.unsplash.com/premium_photo-1690587673708-d6ba8a1579a5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTEzfHxhdmF0YXJ8ZW58MHx8MHx8fDA%3D"
-                />
-              </ListItemAvatar>
-              <ListItemAvatar>
-                <Avatar
-                  alt={"Vesna"}
-                  src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTczfHxhdmF0YXJ8ZW58MHx8MHx8fDA%3D"
-                />
-              </ListItemAvatar>
-              <ListItemText primary="Boštja in Vesna" secondary="začetnika" />
-              <ListItemText primary="prideta" />
-            </ListItem>
-          </List>
+          <DriversTable />
         </Section>
       </Container>
     </Layout>
