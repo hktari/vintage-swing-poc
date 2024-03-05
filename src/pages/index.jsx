d1a91045c8305af17347c3b78aa1c6f39c7a46d3
hktari <bkamnik1995@gmail.com>
2024-03-05 04:22:44 +0100

    add couples list item

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -81,6 +81,28 @@ export default function Index() {
                 <ListItemText primary={user.status} />
               </ListItem>
             ))}
+            <ListItem
+              secondaryAction={
+                <IconButton edge="end" aria-label="delete">
+                  <CallIcon />
+                </IconButton>
+              }
+            >
+              <ListItemAvatar>
+                <Avatar
+                  alt={"Boštjan"}
+                  src="https://plus.unsplash.com/premium_photo-1690587673708-d6ba8a1579a5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTEzfHxhdmF0YXJ8ZW58MHx8MHx8fDA%3D"
+                />
+              </ListItemAvatar>
+              <ListItemAvatar>
+                <Avatar
+                  alt={"Vesna"}
+                  src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTczfHxhdmF0YXJ8ZW58MHx8MHx8fDA%3D"
+                />
+              </ListItemAvatar>
+              <ListItemText primary="Boštja in Vesna" secondary="začetnika" />
+              <ListItemText primary="prideta" />
+            </ListItem>
           </List>
         </Grid>
       </Container>
