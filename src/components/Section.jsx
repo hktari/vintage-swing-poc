afe1d0bcbb319a27b976c0bce8f1b21241528301
hktari <bkamnik1995@gmail.com>
2024-03-05 04:27:32 +0100

    extraction Section

---
+++ src/components/Section.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Grid from "@mui/material/Grid"
+import Typography from "@mui/material/Typography"
+
+const Section = ({ title, children }) => {
+  return (
+    <Grid item xs={12} md={6}>
+      <Typography sx={{ mt: 4, mb: 2 }} variant="h5" component="div">
+        {title}
+      </Typography>
+      {children}
+    </Grid>
+  )
+}
+
+export default Section
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,6 +21,7 @@ import Grid from "@mui/material/Grid"
 import CallIcon from "@mui/icons-material/Call"
 
 import users from "../../content/users.json"
+import Section from "../components/Section"
 
 export default function Index() {
   return (
@@ -60,11 +61,7 @@ export default function Index() {
             </TableBody>
           </Table>
         </TableContainer>
-
-        <Grid item xs={12} md={6}>
-          <Typography sx={{ mt: 4, mb: 2 }} variant="h5" component="div">
-            Prijavljeni
-          </Typography>
+        <Section title={"Prijavljeni"}>
           <List dense={false}>
             {users.map(user => (
               <ListItem
@@ -104,7 +101,49 @@ export default function Index() {
               <ListItemText primary="prideta" />
             </ListItem>
           </List>
-        </Grid>
+        </Section>
+
+        <Section title={"Prevozi"}>
+          <List dense={false}>
+            {users.map(user => (
+              <ListItem
+                secondaryAction={
+                  <IconButton edge="end" aria-label="delete">
+                    <CallIcon />
+                  </IconButton>
+                }
+              >
+                <ListItemAvatar>
+                  <Avatar alt={user.name} src={user.imgSrc} />
+                </ListItemAvatar>
+                <ListItemText primary={user.name} secondary={user.level} />
+                <ListItemText primary={user.status} />
+              </ListItem>
+            ))}
+            <ListItem
+              secondaryAction={
+                <IconButton edge="end" aria-label="delete">
+                  <CallIcon />
+                </IconButton>
+              }
+            >
+              <ListItemAvatar>
+                <Avatar
+                  alt={"Boštjan"}
+                  src="https://plus.unsplash.com/premium_photo-1690587673708-d6ba8a1579a5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTEzfHxhdmF0YXJ8ZW58MHx8MHx8fDA%3D"
+                />
+              </ListItemAvatar>
+              <ListItemAvatar>
+                <Avatar
+                  alt={"Vesna"}
+                  src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTczfHxhdmF0YXJ8ZW58MHx8MHx8fDA%3D"
+                />
+              </ListItemAvatar>
+              <ListItemText primary="Boštja in Vesna" secondary="začetnika" />
+              <ListItemText primary="prideta" />
+            </ListItem>
+          </List>
+        </Section>
       </Container>
     </Layout>
   )
