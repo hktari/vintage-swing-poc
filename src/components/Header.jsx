44e26bc522f5fd098c2dd142098218ad7125a328
hktari <bkamnik1995@gmail.com>
2024-03-12 09:59:23 +0100

    add header buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,41 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { Box } from "@mui/material"
+import { Box, IconButton } from "@mui/material"
 import { StaticImage } from "gatsby-plugin-image"
 import logo from "../images/logo.png"
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos"
+import MenuIcon from "@mui/icons-material/Menu"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <Box
-      sx={{
-        py: 2,
-        px: 4,
-        display: "flex",
-        justifyContent: "end",
-        alignItems: "center",
-        bgcolor: "primary.main",
-      }}
-    >
-      <img src={logo} height={50} alt="vintage swing logo" />
-      <Box sx={{ height: "75px" }} color="secondary"></Box>
-    </Box>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  const topLevelLocation = true
+
+  return (
+    <header>
+      <Box
+        sx={{
+          py: 2,
+          px: 4,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          bgcolor: "primary.main",
+          height: "75px",
+        }}
+      >
+        {topLevelLocation ? (
+          <IconButton aria-label="side menu" color="primary" size="large">
+            <MenuIcon sx={{color: 'primary.contrastText'}}/>
+          </IconButton>
+        ) : (
+          <IconButton aria-label="navigate back">
+            <ArrowBackIosIcon sx={{color: 'primary.contrastText'}}/>
+          </IconButton>
+        )}
+
+        <img src={logo} height={50} alt="vintage swing logo" />
+      </Box>
+    </header>
+  )
+}
 
 export default Header
