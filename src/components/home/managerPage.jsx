a8e74b5ad4d919714dd48ea28817d7a4ce41842c
hktari <bkamnik1995@gmail.com>
2024-03-13 13:30:23 +0100

    add sample list and json

--- src/components/home/managerPage.jsx
+++ src/components/home/managerPage.jsx
@@ -1,13 +1,57 @@
-import { Container, Typography } from "@mui/material"
 import React from "react"
-
+import users from "../../../content/unverified-users.json"
+import {
+  Container,
+  Avatar,
+  ListItemAvatar,
+  Divider,
+  List,
+  Box,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material"
 const ManagerPage = ({ location, data }) => {
   return (
-    <Container>
+    <Container sx={{ py: 4 }}>
       {" "}
-      <Typography variant="h4" component="h1">
-        Upravljanje
-      </Typography>
+      <Box component={"section"}>
+        <Typography variant="h4" component="h2">
+          Novi uporabniki
+        </Typography>
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        >
+          {users.map(user => (
+            <React.Fragment>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Brunch this weekend?"
+                  secondary={
+                    <React.Fragment>
+                      <Typography
+                        sx={{ display: "inline" }}
+                        component="span"
+                        variant="body2"
+                        color="text.primary"
+                      >
+                        Ali Connors
+                      </Typography>
+                      {" — I'll be in your neighborhood doing errands this…"}
+                    </React.Fragment>
+                  }
+                />
+              </ListItem>
+              <Divider variant="inset" component="li" />
+            </React.Fragment>
+          ))}
+        </List>
+      </Box>
     </Container>
   )
 }
