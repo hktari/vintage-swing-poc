5c647074f32baf64a6686b19cae18ac323f1a4f7
hktari <bkamnik1995@gmail.com>
2024-03-14 11:20:57 +0100

    update user.json interface

--- src/interface/User.ts
+++ src/interface/User.ts
@@ -1,6 +1,12 @@
 export default interface User {
   name: string
   level: string
-  status: string
+  status: UserEventStatus
   imgSrc: string
+}
+
+export type UserEventStatus = {
+  signedUp: boolean
+  lookingForRide: boolean
+  lookingForPartner: boolean
 }
